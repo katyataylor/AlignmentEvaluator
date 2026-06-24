17bc7927d79793fa4530c1157a88eb31c68a78a7
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 19:41:49 -0500

    Add array values and calculations

--- chartGame.js
+++ chartGame.js
@@ -1,4 +1,10 @@
 window.addEventListener(`DOMContentLoaded`, () => {
+
+    // Slider listeners
+    const xSlider1 = document.querySelector(`.x-slider-1`);
+    const xSlider2 = document.querySelector(`.x-slider-2`);
+    const ySlider1 = document.querySelector(`.y-slider-1`);
+    const ySlider2 = document.querySelector(`.y-slider-2`);
     
     // Defines initial variable containers
     let idScore = 0;
@@ -18,36 +24,40 @@ window.addEventListener(`DOMContentLoaded`, () => {
     let xScore = totalXPoints === 0 ? 5 : (idScore / totalXPoints) * 10; // MISSING 'let'
     let yScore = totalYPoints === 0 ? 5 : (benefitScore / totalYPoints) * 10; // MISSING 'let'
 
-    let initialXPercent = (xScore / 10) * 98 +1;
-    let initialYPercent = (yScore / 10) * 98 +1;
+    let initialXPercentage = (idScore / 20) * 98 + 1;
+    let initialYPercentage = (benefitScore / 20) * 98 +1;
 
-    playerDot.style.left = `${initialXPercent}%`;
-    playerDot.style.top = `${initialYPercent}%`;
+    playerDot.style.left = `${initialXPercentage}%`;
+    playerDot.style.top = `${initialYPercentage}%`;
 
-    // Listen for X-axis changes
-    xSlider.addEventListener("input", () => {
-        let currentScore = Number(xSlider.value);
-        idScore = currentScore;
-        superegoScore = 10 - currentScore;
-        
-        let totalXPoints = idScore + superegoScore;
-        let xScore = totalXPoints === 0 ? 5 : (idScore / totalXPoints) * 10;
-        
-        let xPercentage = (xScore / 10) * 98 + 1;
+    const updateChart = () => {
+
+        // Creates an array of current X slider values
+        const xValues = [Number(xSlider1.value), Number(xSlider2.value)];
+        let idScore = 0; 
+        for (let i = 0; i < xValues.length; i++) { 
+            idScore += xValues[i];
+        }
+        let maxXPoints = xValues.length * 10;
+        let xPercentage = (idScore / maxXPoints) * 98 + 1;
+
+    
+        // Creates an array of current Y slider values
+        const yValues = [Number(ySlider1.value), Number(ySlider2.value)];
+        let benefitScore = 0; 
+        for (let i = 0; i < yValues.length; i++) { 
+            benefitScore += yValues[i];
+        }
+        let maxXYoints = yValues.length * 10;
+        let yPercentage = (benefitScore / maxXYoints) *98 + 1;
+
         playerDot.style.left = `${xPercentage}%`;
-    });
-
-    // Listen for Y-axis changes
-    ySlider.addEventListener("input", () => {
-        let currentScore = Number(ySlider.value);
-        benefitScore = currentScore;
-        harmScore = 10 - currentScore;
-        
-        let totalYPoints = harmScore + benefitScore;
-        let yScore = totalYPoints === 0 ? 5 : (benefitScore / totalYPoints) * 10;
-        
-        let yPercentage = (yScore / 10) * 98 + 1;
         playerDot.style.top = `${yPercentage}%`;
-    });
+    };
+
+    xSlider1.addEventListener("input", updateChart);
+    xSlider2.addEventListener("input", updateChart);
+    ySlider1.addEventListener("input", updateChart);
+    ySlider2.addEventListener("input", updateChart);
 
 });
